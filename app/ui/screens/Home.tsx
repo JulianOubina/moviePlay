940be289ee92b80a43b0e27532952b55d71bf160
Juanchi <juanchicanestrari@gmail.com>
2024-07-16 00:27:06 -0300

    ADAPTAR TODO EL HOME A GENRESCROLL

--- app/ui/screens/Home.tsx
+++ app/ui/screens/Home.tsx
@@ -39,134 +39,24 @@ const genres = [
   "Horror", "Music", "Mystery", "Romance", "Science Fiction",
   "TV Movie", "Thriller", "War", "Western"
 ];
+const sortedGenres = genres.sort(() => Math.random() - 0.5);
 
-const [randomGenre1, randomGenre2, randomGenre3] = genres.sort(() => 0.5 - Math.random()).slice(0, 3);
 const { width: screenWidth } = Dimensions.get('window');
 
 function Home() {
   const [loading, setLoading] = React.useState<boolean>(true);
   const user = React.useContext(UserContext);
-  
-  const [movies, setMovies] = React.useState<Movie[]>([]);
-  const [moviesGenres2, setMoviesGenres2] = React.useState<Movie[]>([]);
-  const [moviesGenres3, setMoviesGenres3] = React.useState<Movie[]>([]);
-
   const [carrouselPhotos, setCarrouselPhotos] = React.useState<Carrousel[]>([]);
-
-  const [genreTitles, setGenreTitles] = React.useState({ genre1: '', genre2: '', genre3: '' });
-  
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
-  
-  const [page, setPage] = React.useState(1);
-  const [pageGenre2, setPageGenre2] = React.useState(1);
-  const [pageGenre3, setPageGenre3] = React.useState(1);
 
   React.useEffect(() => {
     setTimeout(() => {
       fetchCarrousel();
-      fetchMoviesGenres1(1);
-      fetchMoviesGenres2(1);
-      fetchMoviesGenres3(1);
       setLoading(false);
     }, 4000);
   }, []);
 
-  const fetchMoviesGenres1 = async (pageNumber: any) => {
-    try {
-      const token = await AsyncStorage.getItem('sessionToken');
   
-      const response = await axios.get('https://dai-movieapp-api.onrender.com/movies', {
-        params: {
-          page: pageNumber,
-          genre: randomGenre1,
-        },
-        headers: {
-          'Authorization': `Bearer ${token}`,
-        },
-      });
-
-      setMovies((prevMovies) => [...prevMovies, ...response.data.results]);
-      setGenreTitles(prev => ({ ...prev, genre1: randomGenre1 }));
-      setLoading(false);
-      setPage(pageNumber + 1);
-    } catch (error: any) {
-      switch (error.response.status) {
-        case 403:
-          await getNewTokens();
-          fetchMoviesGenres1(pageNumber);
-          break;
-        default:
-          console.log(error);
-          break;
-      }
-      setLoading(false);
-    }
-  };
-
-  const fetchMoviesGenres2 = async (pageNumber: any) => {
-    try {
-      const token = await AsyncStorage.getItem('sessionToken');
-  
-      const response = await axios.get('https://dai-movieapp-api.onrender.com/movies', {
-        params: {
-          page: pageNumber,
-          genre: randomGenre2,
-        },
-        headers: {
-          'Authorization': `Bearer ${token}`,
-        },
-      });
-
-      setMoviesGenres2((prevMovies) => [...prevMovies, ...response.data.results]);
-      setGenreTitles(prev => ({ ...prev, genre2: randomGenre2 }));
-      setLoading(false);
-      setPageGenre2(pageNumber + 1);
-    } catch (error: any) {
-      switch (error.response.status) {
-        case 403:
-          await getNewTokens();
-          fetchMoviesGenres2(pageNumber);
-          break;
-        default:
-          console.log(error);
-          break;
-      }
-      setLoading(false);
-    }
-  };
-
-  const fetchMoviesGenres3 = async (pageNumber: any) => {
-    try {
-      const token = await AsyncStorage.getItem('sessionToken');
-  
-      const response = await axios.get('https://dai-movieapp-api.onrender.com/movies', {
-        params: {
-          page: pageNumber,
-          genre: randomGenre3,
-        },
-        headers: {
-          'Authorization': `Bearer ${token}`,
-        },
-      });
-
-      setMoviesGenres3((prevMovies) => [...prevMovies, ...response.data.results]);
-      setGenreTitles(prev => ({ ...prev, genre3: randomGenre3 }));
-      setLoading(false);
-      setPageGenre3(pageNumber + 1);
-    } catch (error: any) {
-      switch (error.response.status) {
-        case 403:
-          await getNewTokens();
-          fetchMoviesGenres3(pageNumber);
-          break;
-        default:
-          console.log(error);
-          break;
-      }
-      setLoading(false);
-    }
-  };
-
   const fetchCarrousel = async () =>{
     try {
       const token = await AsyncStorage.getItem('sessionToken');
@@ -246,6 +136,7 @@ function Home() {
     navigation.navigate('MovieDetail', { movieId: movieId });
   };
 
+  
   return (
     <View style={styles.container}>
       <NavBar />
@@ -263,76 +154,16 @@ function Home() {
               contentContainerStyle={styles.carrouselSection}
               renderItem={({ item }) => (
                 <TouchableOpacity onPress={() => handleMoviePress(item.idMovie)}>
-                <View style={styles.imageContainer}>
+                  <View style={styles.imageContainer}>
                     <Image source={{ uri: item.image }} style={styles.image} />
                   </View>
                 </TouchableOpacity>
-              )} />
-          </View>
-          <View style={styles.flatListSection}>
-            <Text style={styles.title}>{genreTitles.genre1}</Text>
-            <FlatList
-              data={movies}
-              keyExtractor={(item) => item.idMovie}
-              horizontal
-              showsHorizontalScrollIndicator={false}
-              contentContainerStyle={styles.flatListContainer}
-              renderItem={({ item }) => (
-                <TouchableOpacity onPress={() => handleMoviePress(item.idMovie )}>
-                  <View style={styles.movieContainer}>
-                    <Image source={{ uri: item.images }} style={styles.movieImage} />
-                  </View>
-                </TouchableOpacity>
               )}
-              onEndReached={() => {
-                fetchMoviesGenres1(page);
-              }}
-              onEndReachedThreshold={0.5}
             />
           </View>
-          <View style={styles.flatListSection}>
-            <Text style={styles.title}>{genreTitles.genre2}</Text>
-            <FlatList
-              data={moviesGenres2}
-              keyExtractor={(item) => item.idMovie}
-              horizontal
-              showsHorizontalScrollIndicator={false}
-              contentContainerStyle={styles.flatListContainer}
-              renderItem={({ item }) => (
-                <TouchableOpacity onPress={() => handleMoviePress(item.idMovie)}>
-                  <View style={styles.movieContainer}>
-                    <Image source={{ uri: item.images }} style={styles.movieImage} />
-                  </View>
-                </TouchableOpacity>
-              )}
-              onEndReached={() => {
-                fetchMoviesGenres2(pageGenre2);
-              }}
-              onEndReachedThreshold={0.5}
-            />
-          </View>
-          <View style={styles.flatListSection}>
-            <Text style={styles.title}>{genreTitles.genre3}</Text>
-            <FlatList
-              data={moviesGenres3}
-              keyExtractor={(item) => item.idMovie}
-              horizontal
-              showsHorizontalScrollIndicator={false}
-              contentContainerStyle={styles.flatListContainer}
-              renderItem={({ item }) => (
-                <TouchableOpacity onPress={() => handleMoviePress(item.idMovie)}>
-                  <View style={styles.movieContainer}>
-                    <Image source={{ uri: item.images }} style={styles.movieImage} />
-                  </View>
-                </TouchableOpacity>
-              )}
-              onEndReached={() => {
-                fetchMoviesGenres3(pageGenre3);
-              }}
-              onEndReachedThreshold={0.5}
-            />
-          </View>
-          <GenreScroll genreTitle="History" handleMoviePress={handleMoviePress}></GenreScroll>
+          <GenreScroll genreTitle={sortedGenres[0]} handleMoviePress={handleMoviePress} />
+          <GenreScroll genreTitle={sortedGenres[1]} handleMoviePress={handleMoviePress} />
+          <GenreScroll genreTitle={sortedGenres[2]} handleMoviePress={handleMoviePress} />
         </ScrollView>
       )}
     </View>
